45662bc927cff2f63a5654cbce3db8badf4758d8
Anshika2608 <anshika2210142@akgec.ac.in>
2023-10-17 21:42:00 +0530

    added first-list

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+     <h1 >My Favourite Places</h1>
+    <ul>
+      <li>Firozabad</li>
+      <li>Jaipur</li>
+      <li>Udaipur</li>
+    </ul></>
+    )
+ 
+
+}
+
+export default App

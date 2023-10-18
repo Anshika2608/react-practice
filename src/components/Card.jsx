e1ed3a5c4d70bfd6d289517f84123b7b3a482559
Anshika2608 <anshika2210142@akgec.ac.in>
2023-10-18 20:49:47 +0530

    completed props task

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom'
 import './img.css'
 function Card(props){
     return (
-        <div>
+        <> 
+            <div className="contact">     
+            <div className="upper">
             <h2>{props.name}</h2>
             <img  className="img1" src={props.src} alt="Kareena Kapoor" />
+            </div>
+            <div className="lower">
             <p>{props.tel}</p>
             <p>{props.email}</p>
-        </div>
+            </div>
+            </div>
+        </ >
     )
 }
 export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -14,10 +14,10 @@ import Card from './components/Card'
 // )
 ReactDOM.render(
   <div>
-   <h1>My Contacts</h1>
-    <Card name="kareena kapoor"  tel="+123 456 789" email="@kareenakapoor.com"  src="https://i2.cinestaan.com/image-bank/1500-1500/111001-112000/111406.jpg"/>
-    <Card  name="salman khan"  tel="+123 467 689" email="@salmankhan.com" src="https://th.bing.com/th/id/OIP.HFH356lCZZNvkC0S2KQHTAHaNK?w=115&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7"/>
-    <Card name="shahrukh khan" tel="+123 454 789" email="@shahrukhkhan.com" src="https://www.masala.com/public/images/2019/11/11/Shah-Rukh-Khan-Baadhshah-Of-Bollywood.jpg" />
+    <h1>MY CONTACTS</h1>
+    <Card name="KAREENA  KAPOOR"  tel="+123 456 789" email="@kareenakapoor.com"  src="https://i2.cinestaan.com/image-bank/1500-1500/111001-112000/111406.jpg"/>
+    <Card  name="SALMAN KHAN"  tel="+123 467 689" email="@salmankhan.com" src="https://th.bing.com/th/id/OIP.HFH356lCZZNvkC0S2KQHTAHaNK?w=115&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7"/>
+    <Card name="SHAHRUKH KHAN" tel="+123 454 789" email="@shahrukhkhan.com" src="https://www.masala.com/public/images/2019/11/11/Shah-Rukh-Khan-Baadhshah-Of-Bollywood.jpg" />
   </div>
 ,
   document.getElementById("root")
